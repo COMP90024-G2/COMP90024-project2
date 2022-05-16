fb7a7c1a93f096b0b90381432d6c102537af5493
Claire Zhang <clairezhang0109@gmail.com>
2022-05-17 00:13:06 +1000

    change file name

--- preprocess_json.py
+++ preprocess_json.py
@@ -5,12 +5,12 @@ import json
 
 
 if __name__ == "__main__":
-    with open("data/final_output.json") as f:
+    with open("data/output.json") as f:
         data = json.load(f)
         new_data = data['rows'][0]['doc']['historical_analysis']
         
         
-    with open('data/processed_trial.json', 'w') as fp:
+    with open('data/processed.json', 'w') as fp:
         json.dump(new_data, fp)
                 
 
